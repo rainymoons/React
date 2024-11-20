567a84d9861ded6dfb42c66f470cdf88b2bc1955
rainymoon31 <rainymoon31@gmail.com>
2024-11-20 09:59:03 +0900

    react todo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import Todo from "./components/todo/Todo";
 
-function App() {
+export default function App() {
+  const todoItemList = [
+    { id: 0, isDone: true, task: "ref", dueDate: "2024-11-19" },
+    { id: 1, isDone: false, task: "ContextAPI", dueDate: "2024-11-20" },
+  ];
+
+  const [todoList, setTodoList] = useState(todoItemList);
+
+  // 하나만 바꿀 것이기 때문에 event가 필요.
+  const onDoneHandler = (event) => {
+    const checkedDoneId = parseInt(event.target.value);
+    const isChecked = event.target.checked;
+
+    // 배열 카피를 해서 새로운 메모리를 생성 -> isDone의 값을 바꿀 수 없다. 그리고 최신 아이템 확신 못함.
+    //setTodoList([...todoList]);
+    // ()안에 함수를 넣어버린다.
+    // 앞에는 이전까지 관리하던 가장 최신의 state
+    // map -> 새로운 메모리주소를 참조하는 새 배열을 만들어서 반환함. [...]을 사용할 필요가 없음.
+    setTodoList((prevTodoItemList) =>
+      prevTodoItemList.map((todo) => {
+        if (todo.id === checkedDoneId) {
+          todo.isDone = isChecked; // 체크를 했던 todo는 값이 바뀌어서 반환된다.
+        }
+        return todo;
+      })
+    );
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ul>
+      {todoList.map((todo) => (
+        <Todo key={todo.id} todo={todo} onClickDoneHandler={onDoneHandler} />
+      ))}
+    </ul>
   );
 }
-
-export default App;
